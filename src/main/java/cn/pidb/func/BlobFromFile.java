3f59e2c4a1536e97def5a7507b1f079a21bfd254
zhangzl419 <zhangzl419@163.com>
2018-07-19 15:28:58 +0800

    add a custom function blobFromFile(String localPath) and its test class

---
+++ src/main/java/cn/pidb/func/BlobFromFile.java
@@ -0,0 +1,41 @@
+/**
+ * a user defined function for pidb which supports generate a Blob object from a local file.
+ * create by zhangzl419 on 2018-07-16 at cnic
+ */
+package cn.pidb.func;
+
+import org.neo4j.procedure.Description;
+import org.neo4j.procedure.Name;
+import org.neo4j.procedure.UserFunction;
+import org.neo4j.values.storable.Blob$;
+import org.neo4j.values.storable.Blob;
+import java.io.File;
+
+public class BlobFromFile
+{
+    @UserFunction
+    @Description("cn.pidb.func.blobFromFile('/path/to/file'), generate a blob object from the given file")
+    public Blob blobFromFile(@Name("filePath") String filePath)
+    {
+        Blob blob = null;
+
+        if (filePath == null || filePath == "")
+        {
+            blob = null;
+        }
+        else {
+            File localFile = new File(filePath);
+
+            blob = Blob$.MODULE$.fromFile(localFile);
+            System.out.println(localFile);
+        }
+
+        return blob;
+    }
+
+    public static void main(String[] args)
+    {
+        Blob blob = new BlobFromFile().blobFromFile("./test.png");
+        System.out.println(blob);
+    }
+}
